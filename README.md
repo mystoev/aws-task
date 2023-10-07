d95d23887205081a1c10592e6c725ba580dbe5ea
Martin Stoev <martin.y.stoev@gmail.com>
2023-10-07 12:59:20 +0300

    sign private key. update readme

--- README.md
+++ README.md
@@ -42,3 +42,9 @@ openssl genrsa -out key.pem 2048
 openssl req -new -sha256 -key key.pem -out csr.csr
 openssl req -x509 -sha256 -days 365 -key key.pem -in csr.csr -out certificate.pem
 ```
+
+## To run the project
+
+```sh
+npm start
+```
--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-import { X509Certificate } from "crypto";
+import { X509Certificate, generateKeyPairSync, createSign } from "crypto";
 import fs from "fs";
 
-const x509 = new X509Certificate(fs.readFileSync("cert.pem"));
+const CERTIFICATE_FILE = "cert.pem";
+const KEY_ALGORITHM = "rsa";
+const SIGN_ALGORITHM = "sha256";
+
+const x509 = new X509Certificate(fs.readFileSync(CERTIFICATE_FILE));
 
 const commonName = x509.subject.split("CN=")[1].split("\n")[0];
-const publicKey = x509.publicKey.export({
+const certPublicKey = x509.publicKey.export({
   format: "pem",
   type: "spki",
 });
 
 console.log(commonName);
-console.log(publicKey);
+console.log(certPublicKey);
+
+const { privateKey } = generateKeyPairSync(KEY_ALGORITHM, {
+  modulusLength: 2048,
+});
+
+const sign = createSign(SIGN_ALGORITHM);
+sign.update(certPublicKey);
+sign.end();
+
+const signature = sign.sign(privateKey);
+console.log("Signature: ", signature);
