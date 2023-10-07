1744caea0a064dac7161cc0e8ac0a31e503a8ac7
Martin Stoev <martin.y.stoev@gmail.com>
2023-10-07 13:23:02 +0300

    read from s3

--- index.js
+++ index.js
@@ -1,28 +1,62 @@
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { X509Certificate, generateKeyPairSync, createSign } from "crypto";
-import fs from "fs";
 
 const CERTIFICATE_FILE = "cert.pem";
+const BUCKET_NAME = "d71c3340-64f8-11ee-8c99-0242ac120002";
 const KEY_ALGORITHM = "rsa";
 const SIGN_ALGORITHM = "sha256";
 
-const x509 = new X509Certificate(fs.readFileSync(CERTIFICATE_FILE));
+const readCertFromS3 = async () => {
+  const client = new S3Client({});
+  const command = new GetObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: CERTIFICATE_FILE,
+  });
 
-const commonName = x509.subject.split("CN=")[1].split("\n")[0];
-const certPublicKey = x509.publicKey.export({
-  format: "pem",
-  type: "spki",
-});
+  const response = await client.send(command);
+  return response.Body.transformToByteArray();
+};
 
-console.log(commonName);
-console.log(certPublicKey);
+const extractCertInfo = (cert) => {
+  const x509 = new X509Certificate(cert);
+  const commonName = x509.subject.split("CN=")[1].split("\n")[0];
 
-const { privateKey } = generateKeyPairSync(KEY_ALGORITHM, {
-  modulusLength: 2048,
-});
+  const certPublicKey = x509.publicKey.export({
+    format: "pem",
+    type: "spki",
+  });
 
-const sign = createSign(SIGN_ALGORITHM);
-sign.update(certPublicKey);
-sign.end();
+  return { commonName, certPublicKey };
+};
 
-const signature = sign.sign(privateKey);
-console.log("Signature: ", signature);
+const signData = (data) => {
+  const { privateKey } = generateKeyPairSync(KEY_ALGORITHM, {
+    modulusLength: 2048,
+  });
+
+  const sign = createSign(SIGN_ALGORITHM);
+  sign.update(data);
+  sign.end();
+
+  return sign.sign(privateKey);
+};
+
+const writeToDynamo = async (data, publicKey) => {
+  throw Error("not implemented, yet");
+};
+
+export const main = async () => {
+  try {
+    const cert = await readCertFromS3();
+    const { certPublicKey, commonName } = extractCertInfo(cert);
+
+    const signature = signData(certPublicKey);
+
+    await writeToDynamo(signature, certPublicKey);
+    console.log("Success!");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+main();
